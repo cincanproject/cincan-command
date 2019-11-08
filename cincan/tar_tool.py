d15b44d5b591f72df641f5828af31b4dd64bbcad
Rauli Kaksonen <rauli.kaksonen@icloud.com>
2019-11-08 11:26:42 +0200

    Split long download method into three methods

--- cincan/tar_tool.py
+++ cincan/tar_tool.py
@@ -6,7 +6,7 @@ import tarfile
 import tempfile
 import timeit
 from logging import Logger
-from typing import Dict, Optional, List
+from typing import Dict, Optional, List, Iterable
 
 from docker.models.containers import Container
 
@@ -62,31 +62,15 @@ class TarTool:
         # ... just strip it and put all to work dir
         host_file = pathlib.Path((file_name[1:] if file_name.startswith('/') else file_name).replace(':', '_'))
 
+        # get the tar ball for the file
         get_arc_start = timeit.default_timer()
         chunks, stat = self.container.get_archive(file_name)
+        check = self.__check_for_download(host_file, stat)
 
-        up_stat = self.upload_stats.get(host_file.as_posix())
+        if not check:
+            return  # do not attempt download
 
-        # NOTE: for directories (?) size from container is 4096 -> mismatch for directories!!!
-        up_down_size_mismatch = False
-        if up_stat and 'size' in stat:
-            down_size = int(stat['size'])
-            up_size = up_stat[0]
-            up_down_size_mismatch = up_size != down_size
-            if up_down_size_mismatch:
-                self.logger.debug(f"size {host_file.as_posix()} change {up_size} -> {down_size}")
-
-        if (not up_down_size_mismatch) and up_stat and 'mtime' in stat:
-            down_time_s = stat['mtime']  # seconds + timezone
-            up_time = datetime.datetime.fromtimestamp(int(up_stat[1]))
-            up_time_s = up_time.strftime(self.time_format_seconds)  # seconds
-            time_now_s = datetime.datetime.now().strftime(self.time_format_seconds)
-            if down_time_s.startswith(up_time_s) and up_time_s != time_now_s:
-                # down_time is seconds, up_time has more precision
-                self.logger.debug(f"timestamp {host_file.as_posix()} not updated {down_time_s}")
-                return
-            self.logger.debug(f"timestamp {host_file.as_posix()} updated {up_time_s} -> {down_time_s}")
-
+        # read the tarball into temp file
         tmp_tar = tempfile.TemporaryFile()
         for c in chunks:
             # self.logger.debug(f"chunk of {len(c)} bytes")
@@ -116,38 +100,28 @@ class TarTool:
             elif tf.isfile() and host_file.exists():
                 # compare by hash, if should override local file
                 tf_data = down_tar.extractfile(tf)
-                candi_file = pathlib.Path(host_file.as_posix() + '_TEMP')
-                self.logger.debug(f"creating temp file {candi_file.as_posix()}")
+                temp_file = pathlib.Path(host_file.as_posix() + '_TEMP')
+                self.logger.debug(f"creating temp file {temp_file.as_posix()}")
+
+                # calculate hash for file from tar, copy it to temp file
                 if host_file.parent:
                     host_file.parent.mkdir(parents=True, exist_ok=True)
-                candi_f = candi_file.open("wb")
-                md5sum = hashlib.md5()
-                f_chunk = tf_data.read(2048)
-                while f_chunk:
-                    candi_f.write(f_chunk)
-                    md5sum.update(f_chunk)
-                    f_chunk = tf_data.read(2048)
-                candi_f.close()
-                tf_digest = md5sum.hexdigest()
+                with temp_file.open("wb") as f:
+                    tf_digest = self.__read_with_hash(tf_data.read, f.write)
 
                 # calculate hash for existing file
                 with host_file.open("rb") as f:
-                    md5sum = hashlib.md5()
-                    f_chunk = f.read(2048)
-                    while f_chunk:
-                        md5sum.update(f_chunk)
-                        f_chunk = f.read(2048)
-                    candi_digest = md5sum.hexdigest()
+                    host_digest = self.__read_with_hash(f.read)
 
-                if tf_digest == candi_digest:
+                if tf_digest == host_digest:
                     self.logger.debug(f"identical file {host_file.as_posix()} md5 {tf_digest}, no action")
-                    candi_file.unlink()
+                    temp_file.unlink()
                     continue
 
-                self.logger.debug(f"file {host_file.as_posix()} md5 in container {tf_digest}, in host {candi_digest}")
+                self.logger.debug(f"file {host_file.as_posix()} md5 in container {tf_digest}, in host {host_digest}")
                 self.logger.info(f"copy out and replace {host_file.as_posix()}")
                 host_file.unlink()
-                candi_file.rename(host_file)
+                temp_file.rename(host_file)
             elif tf.isdir() and host_file.is_file():
                 raise Exception(f"mkdir {host_file.as_posix()} failed, a file with that name exists")
             elif tf.isdir() and host_file.is_dir():
@@ -156,3 +130,38 @@ class TarTool:
                 self.logger.info(f"mkdir {host_file.as_posix()}")
                 host_file.mkdir(parents=True, exist_ok=True)
         tmp_tar.close()
+
+    def __check_for_download(self, host_file: pathlib.Path, stat: Dict) -> bool:
+        up_stat = self.upload_stats.get(host_file.as_posix())
+
+        # NOTE: for directories (?) size from container is 4096 -> mismatch for directories!!!
+        up_down_size_mismatch = False
+        if up_stat and 'size' in stat:
+            down_size = int(stat['size'])
+            up_size = up_stat[0]
+            up_down_size_mismatch = up_size != down_size
+            if up_down_size_mismatch:
+                self.logger.debug(f"size {host_file.as_posix()} change {up_size} -> {down_size}")
+
+        if (not up_down_size_mismatch) and up_stat and 'mtime' in stat:
+            down_time_s = stat['mtime']  # seconds + timezone
+            up_time = datetime.datetime.fromtimestamp(int(up_stat[1]))
+            up_time_s = up_time.strftime(self.time_format_seconds)  # seconds
+            time_now_s = datetime.datetime.now().strftime(self.time_format_seconds)
+            if down_time_s.startswith(up_time_s) and up_time_s != time_now_s:
+                # down_time is seconds, up_time has more precision
+                self.logger.debug(f"timestamp {host_file.as_posix()} not updated {down_time_s}")
+                return False
+            self.logger.debug(f"timestamp {host_file.as_posix()} updated {up_time_s} -> {down_time_s}")
+
+        return True
+
+    def __read_with_hash(self, read_more, write_to: Optional = None) -> str:
+        md5sum = hashlib.md5()
+        chunk = read_more(2048)
+        while chunk:
+            md5sum.update(chunk)
+            if write_to:
+                write_to(chunk)
+            chunk = read_more(2048)
+        return md5sum.hexdigest()
