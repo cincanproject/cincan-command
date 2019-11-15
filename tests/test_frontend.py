c9811775696a1e521481a3639739fcbcb659aaa7
Rauli Kaksonen <rauli.kaksonen@icloud.com>
2019-11-15 10:41:20 +0200

    Use cincan/env image for unit tests

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -2,9 +2,9 @@ from cincan.frontend import ToolImage
 
 
 def test_run_get_string():
-    tool = ToolImage(image='cincan/test2tool')
-    out = tool.run_get_string(['echo Hello'])
+    tool = ToolImage(image='cincan/env', rm=False)
+    out = tool.run_get_string(['echo', 'Hello'])
     assert out == 'Hello\n'
 
-    out = tool.run_get_string(['echxo Hello'])
-    assert 'echxo: not found' in out
+    out = tool.run_get_string(['echxo', 'Hello'])
+    assert 'No such file or directory' in out
