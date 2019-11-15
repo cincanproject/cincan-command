87bbe4debb75e6a026e25885000c0e66689fc2b1
Rauli Kaksonen <rauli.kaksonen@icloud.com>
2019-11-15 10:41:37 +0200

    Test file upload

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -8,3 +8,10 @@ def test_run_get_string():
 
     out = tool.run_get_string(['echxo', 'Hello'])
     assert 'No such file or directory' in out
+
+
+def test_magic_file_io():
+    tool = ToolImage(image='cincan/env', rm=False)
+    out = tool.run_get_string(['cat', 'samples/source-a.txt'])
+    assert out == 'Source A\n'
+    assert tool.upload_files == {'samples/source-a.txt': 'samples/source-a.txt'}
