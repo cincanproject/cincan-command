22cf0abdd089383045909708b0f827e91a121bd3
Rauli Kaksonen <rauli.kaksonen@icloud.com>
2019-11-15 08:17:47 +0200

    Test also stderr capture

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -5,3 +5,6 @@ def test_run_get_string():
     tool = ToolImage(image='cincan/test2tool')
     out = tool.run_get_string(['echo Hello'])
     assert out == 'Hello\n'
+
+    out = tool.run_get_string(['echxo Hello'])
+    assert 'echxo: not found' in out
