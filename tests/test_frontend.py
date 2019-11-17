6841166d61ede61ba86a676caf237369f6ccd475
Rauli Kaksonen <rauli.kaksonen@icloud.com>
2019-11-17 14:22:19 +0200

    Copy to host file did not work

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -7,14 +7,15 @@ from cincan.frontend import ToolImage
 
 
 def prepare_work_dir(name: str, with_files: List['str']) -> pathlib.Path:
+    src_root = pathlib.Path('samples')
     root = pathlib.Path(name)
     if root.is_dir():
         shutil.rmtree(root, ignore_errors=True)
     root.mkdir()
     for f_name in with_files:
-        f = pathlib.Path(f_name)
-        shutil.copy(f, root / f.name)
-        shutil.copystat(f, root / f.name)
+        src = src_root / f_name
+        shutil.copy(src, root / src.name)
+        shutil.copystat(src, root / src.name)
     return root
 
 
@@ -29,19 +30,19 @@ def test_run_get_string():
 
 def test_magic_file_io():
     tool = ToolImage(image='cincan/env', rm=False)
-    work_dir = prepare_work_dir('_test', ['samples/source-a.txt'])
+    work_dir = prepare_work_dir('_test', ['source-a.txt'])
     out = tool.run_get_string(['cat', '_test/source-a.txt'])
     assert out == 'Source A\n'
     assert tool.upload_files == ['_test/source-a.txt']
     assert tool.download_files == []
 
-    work_dir = prepare_work_dir('_test', ['samples/source-b.txt'])
+    work_dir = prepare_work_dir('_test', ['source-b.txt'])
     out = tool.run_get_string(['cat', '_test/source-b.txt'])
     assert out == 'Source B\n'
     assert tool.upload_files == ['_test/source-b.txt']
     assert tool.download_files == []
 
-    work_dir = prepare_work_dir('_test', ['samples/source-a.txt'])
+    work_dir = prepare_work_dir('_test', ['source-a.txt'])
     tool.run(["sh", "-c", '''cat _test/source-a.txt > _test/test_a.txt'''])
     assert tool.upload_files == ['_test/source-a.txt']
     assert tool.download_files == ['_test/test_a.txt']
@@ -54,7 +55,7 @@ def test_magic_file_io():
 
 def test_many_output_files():
     tool = ToolImage(image='cincan/env', rm=False)
-    work_dir = prepare_work_dir('_test', ['samples/ab.zip'])
+    work_dir = prepare_work_dir('_test', ['ab.zip'])
     tool.run_get_string(['unzip', '-d', '_test', '_test/ab.zip'])
     assert tool.upload_files == ['_test/ab.zip']
     assert tool.download_files == ['_test/source-a.txt', '_test/source-b.txt']
@@ -62,7 +63,7 @@ def test_many_output_files():
 
 def test_explicit_in_out_files():
     tool = ToolImage(image='cincan/env', rm=False)
-    work_dir = prepare_work_dir('_test', ['samples/ab.zip', 'samples/empty.file'])
+    work_dir = prepare_work_dir('_test', ['ab.zip', 'empty.file'])
     tool.input_files = ['_test/ab.zip', '_test/empty.file']
     tool.output_files = ['_test/source-b.txt']
 
