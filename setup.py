961ad5bac2f046b585e9bbd1a21ef1eaa30f58c7
Rauli Kaksonen <rauli.kaksonen@icloud.com>
2019-11-29 17:31:44 +0200

    version 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='cincan-command',
-    version='0.2.0',
+    version='0.2.1',
     author="Rauli Kaksonen",
     author_email="rauli.kaksonen@gmail.com",
     description='Cincan wrapper for dockerized command-line tools',
