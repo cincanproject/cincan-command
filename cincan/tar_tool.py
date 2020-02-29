dcae654457b290a5dd8990a03d7ca45f42b070d2
OUSPG <ouspg@anonymous.local>
2020-02-29 15:47:28 +0200

    typo fix

--- cincan/tar_tool.py
+++ cincan/tar_tool.py
@@ -339,7 +339,7 @@ class TarTool:
                 return True
 
         if up_stat and 'mtime' in stat:
-            # MacOS need some timezone specifications
+            # MacOS needs some timezone specifications
             if sys.platform == "darwin":
                 up_time = datetime.utcfromtimestamp(int(up_stat[1]))
             else:
