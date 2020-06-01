eb3eb35b3f5a6d776050d33dafe4b2ec9b36b61e
Niklas Saari <niklas.saari@tutanota.com>
2020-06-01 15:15:17 +0300

    Make version tests configurable in case image/tags change

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -3,17 +3,23 @@ import pytest
 from asyncio import coroutine
 from unittest import mock
 from cincan.frontend import ToolImage
+from cincan.configuration import Configuration
 from copy import deepcopy
 
-VERSION_DATA = {"name": "cincan/test",
+TEST_IMAGE = "cincan/test:dev"
+TEST_IMAGE_NAME = TEST_IMAGE.rsplit(":", 1)[0]
+TEST_IMAGE_TAG = TEST_IMAGE.rsplit(":", 1)[1]
+DEFAULT_STABLE_TAG = Configuration().default_stable_tag
+
+VERSION_DATA = {"name": TEST_IMAGE_NAME,
                 "versions": {
                     "local": {
                         "version": "1.0",
-                        "tags": ["cincan/test:latest-stable", ]
+                        "tags": [f"{TEST_IMAGE_NAME}:{DEFAULT_STABLE_TAG}", ]
                     },
                     "remote": {
                         "version": "1.0",
-                        "tags": ["latest-stable"]
+                        "tags": [DEFAULT_STABLE_TAG]
                     },
                     "origin": {
                         "version": "1.0",
@@ -37,7 +43,7 @@ def test_image_version_up_to_date(caplog):
     def coroutine_func():
         """Mock coroutine function to have custom result"""
         version_data_copy = deepcopy(VERSION_DATA)
-        version_data_copy["versions"]["remote"]["tags"] = ["latest", "latest-stable"]
+        version_data_copy["versions"]["remote"]["tags"] = ["latest", TEST_IMAGE_TAG]
         coroutine_result = mock.Mock(name="CoroutineResult", return_value=version_data_copy)
         mocked_funk = mock.Mock(name="CoroutineFunction", side_effect=coroutine(coroutine_result))
         mocked_funk.coroutine_result = coroutine_result
@@ -45,9 +51,9 @@ def test_image_version_up_to_date(caplog):
 
     with mock.patch("cincanregistry.ToolRegistry.get_version_by_image_id", return_value="1.0") as mock_ver_id:
         with mock.patch("cincanregistry.ToolRegistry.list_versions", side_effect=coroutine_func()) as mock_list:
-            tool = ToolImage(image="cincan/test:latest", pull=True, rm=False)
+            tool = ToolImage(image=TEST_IMAGE, pull=True, rm=False)
             mock_ver_id.assert_called()
-            mock_list.assert_called_with("cincan/test", only_updates=False)
+            mock_list.assert_called_with(TEST_IMAGE_NAME, only_updates=False)
     pull_msgs = [
         "Your tool is up-to-date with remote. Current version: 1.0\n"
     ]
@@ -67,11 +73,11 @@ def test_image_version_no_info(caplog):
 
     with mock.patch("cincanregistry.ToolRegistry.get_version_by_image_id", return_value="1.0") as mock_ver_id:
         with mock.patch("cincanregistry.ToolRegistry.list_versions", side_effect=coroutine_func()) as mock_list:
-            tool = ToolImage(image="cincan/test:latest", pull=True, rm=False)
+            tool = ToolImage(image=TEST_IMAGE, pull=True, rm=False)
             mock_ver_id.assert_called()
-            mock_list.assert_called_with("cincan/test", only_updates=False)
+            mock_list.assert_called_with(TEST_IMAGE_NAME, only_updates=False)
     pull_msgs = [
-        "No version information available for cincan/test\n"
+        f"No version information available for {TEST_IMAGE_NAME}\n"
     ]
     logs = [l.message for l in caplog.records]
     assert logs[1:] == pull_msgs
@@ -90,10 +96,10 @@ def test_image_version_local_old_tag(caplog):
 
     with mock.patch("cincanregistry.ToolRegistry.get_version_by_image_id", return_value="0.9"):
         with mock.patch("cincanregistry.ToolRegistry.list_versions", side_effect=coroutine_func()) as mock_list:
-            tool = ToolImage(image="cincan/test:latest", pull=True, rm=False)
+            tool = ToolImage(image=TEST_IMAGE, pull=True, rm=False)
     pull_msgs = [
         "You are not using latest locally available version: (0.9 vs 1.0) Latest is "
-        "available with tags 'cincan/test:latest-stable'",
+        f"available with tags '{TEST_IMAGE_NAME}:{DEFAULT_STABLE_TAG}'",
         "Latest local tool is up-to-date with remote. Latest local version: 1.0"
     ]
     logs = [l.message for l in caplog.records]
@@ -116,10 +122,10 @@ def test_image_version_local_outdated(caplog):
 
     with mock.patch("cincanregistry.ToolRegistry.get_version_by_image_id", return_value="1.0"):
         with mock.patch("cincanregistry.ToolRegistry.list_versions", side_effect=coroutine_func()) as mock_list:
-            tool = ToolImage(image="cincan/test:latest", pull=True, rm=False)
+            tool = ToolImage(image=TEST_IMAGE, pull=True, rm=False)
     pull_msgs = [
         "Update available in remote: (1.0 vs. 1.1)"
-        "\nUse 'docker pull cincan/test:latest-stable' to update."
+        f"\nUse 'docker pull {TEST_IMAGE_NAME}:{DEFAULT_STABLE_TAG}' to update."
     ]
     logs = [l.message for l in caplog.records]
     assert logs[1:] == pull_msgs
@@ -132,7 +138,6 @@ def test_image_version_remote_outdated(caplog):
     def coroutine_func():
         """Mock coroutine function to have custom result"""
         version_data_copy = deepcopy(VERSION_DATA)
-        version_data_copy["versions"]["remote"]["tags"] = ["latest", "latest-stable"]
         version_data_copy["versions"]["origin"]["version"] = "1.1"
         version_data_copy["updates"]["remote"] = True
         coroutine_result = mock.Mock(name="CoroutineResult", return_value=version_data_copy)
@@ -142,7 +147,7 @@ def test_image_version_remote_outdated(caplog):
 
     with mock.patch("cincanregistry.ToolRegistry.get_version_by_image_id", return_value="1.0"):
         with mock.patch("cincanregistry.ToolRegistry.list_versions", side_effect=coroutine_func()) as mock_list:
-            tool = ToolImage(image="cincan/test:latest", pull=True, rm=False)
+            tool = ToolImage(image=TEST_IMAGE, pull=True, rm=False)
     pull_msgs = [
         "Your tool is up-to-date with remote. Current version: 1.0\n",
         "Remote is not up-to-date with origin (1.0 vs. 1.1) in 'GitHub'"
@@ -168,9 +173,9 @@ def test_image_version_new_dev_version(caplog):
 
     with mock.patch("cincanregistry.ToolRegistry.get_version_by_image_id", return_value="1.0"):
         with mock.patch("cincanregistry.ToolRegistry.list_versions", side_effect=coroutine_func()) as mock_list:
-            tool = ToolImage(image="cincan/test:latest", pull=True, rm=False)
+            tool = ToolImage(image=TEST_IMAGE, pull=True, rm=False)
     pull_msgs = [
-        "Newer development version available in remote: 1.1 with tags 'dev'"
+        f"Newer development version available in remote: 1.1 with tags 'dev'"
     ]
     logs = [l.message for l in caplog.records]
     assert logs[1:] == pull_msgs
